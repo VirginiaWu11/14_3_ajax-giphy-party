a46bb4b19d8e19e39e6cfd6a3adfab0c0da586b0
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-03-27 12:23:28 -0400

    add html and js files

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("Let's get this party started!");
+
+// const form = document.querySelector("form");
+// const remove = document.querySelector("#remove");
+// const div = document.querySelector("#gifs");
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   searchGiphy();
+// });
+
+// remove.addEventListener('click',function(e){
+//     e.preventDefault();
+//     div.innerHTML="";
+// }
+
+// function searchGiphy(sTerm) {}
+
+$("#search").on("click", function (e) {
+  e.preventDefault();
+  let searchTerm = $("#searchTerm").val();
+
+  async function getGif(searchTerm) {
+    let result = await axios.get("http://api.giphy.com/v1/gifs/search", {
+      params: { q: searchTerm, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
+    });
+    let amount = result.data.data.length;
+    let random = Math.floor(Math.random() * amount);
+    console.log(random);
+    console.log(result.data.data);
+    let gif = result.data.data[random].images.original.url;
+    $("div").append(`<img src="${gif}"></img>`);
+  }
+  getGif(searchTerm);
+});
+
+$("#remove").on("click", function (e) {
+  e.preventDefault();
+  $("div").html("");
+});
