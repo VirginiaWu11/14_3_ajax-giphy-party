409e883a0edd19dc75c7413f1b0acafd4288a2aa
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-03-27 12:41:41 -0400

    delete comments and add s after http to fix gif load error

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 console.log("Let's get this party started!");
 
-// const form = document.querySelector("form");
-// const remove = document.querySelector("#remove");
-// const div = document.querySelector("#gifs");
-
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   searchGiphy();
-// });
-
-// remove.addEventListener('click',function(e){
-//     e.preventDefault();
-//     div.innerHTML="";
-// }
-
-// function searchGiphy(sTerm) {}
-
 $("#search").on("click", function (e) {
   e.preventDefault();
   let searchTerm = $("#searchTerm").val();
 
   async function getGif(searchTerm) {
-    let result = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    let result = await axios.get("https://api.giphy.com/v1/gifs/search", {
       params: { q: searchTerm, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
     });
     let amount = result.data.data.length;
